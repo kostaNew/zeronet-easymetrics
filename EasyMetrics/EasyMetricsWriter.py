fca135a469ca5885ad481b0b2ffb2e50e4631482
Konstantin Ushenin <kostaNew@gmail.com>
2016-03-14 19:47:15 +0500

    Fix for run with other versions of pandas

--- EasyMetrics/EasyMetricsWriter.py
+++ EasyMetrics/EasyMetricsWriter.py
@@ -9,6 +9,8 @@ class EasyMetricsWriter:
         self.config = easy_metrics_config
         self.directory_name = time.strftime("%Y%m%d_%H%M%S", time.gmtime())
 
+        self.is_header_need_set = True
+
         self.data_folder = os.path.join(self.config.folder, self.directory_name)
         if not os.path.exists(self.config.folder):
             os.makedirs(self.config.folder)
@@ -18,9 +20,8 @@ class EasyMetricsWriter:
     def write_site_data(self, site_name, new_frame):
         file = os.path.join(self.data_folder, self.config.site_data_file)
 
-        tmp_ = new_frame.to_csv(index=False)
-
         with open(file, "a+") as fl:
-           fl.write(tmp_)
+           new_frame.to_csv(fl, index=False, header=self.is_header_need_set)
+           self.is_header_need_set = False
 
 
